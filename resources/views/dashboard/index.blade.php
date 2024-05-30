fd72011b9d11ce328cd4f02a21dbfdf1ebe82c1e
Yuike-kun <akbarhuntermob@gmail.com>
2024-05-30 09:44:16 +0800

    [changed] Login background is changed [fix] Dashboard name changed

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -187,12 +187,13 @@
 <body id="body-pd">
     <header class="header" id="header">
         <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
-        <div class="header_img"> <img src="https://i1.sndcdn.com/avatars-000716184301-va46lr-t240x240.jpg" alt=""> </div>
+        <div class="header_img"> <img src="https://i1.sndcdn.com/avatars-000716184301-va46lr-t240x240.jpg" alt="">
+        </div>
     </header>
     <div class="l-navbar" id="nav-bar">
         <nav class="nav">
             <div> <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span
-                        class="nav_logo-name">BBBootstrap</span> </a>
+                        class="nav_logo-name">E - Bansos</span> </a>
                 <div class="nav_list"> <a href="#" class="nav_link active"> <i
                             class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Dashboard</span> </a> <a
                         href="#" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span
